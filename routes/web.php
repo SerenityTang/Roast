85a6de85f56090304b60604b41e34d7318acf13a
SerenityTang <1060684139@qq.com>
2020-05-08 17:47:06 +0800

    Laravel初始化

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
+});*/
+
+Route::group(['namespace' => 'Web'], function () {
+    Route::group(['middleware' => ['auth']], function () {
+        Route::get('/', 'AppController@getApp');
+    });
 });
