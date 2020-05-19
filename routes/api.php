beb81908d79ec25d068d58e27ec75883e5e74978
SerenityTang <1060684139@qq.com>
2020-05-19 16:28:02 +0800

    JavaScript初始化-配置 JavaScript 和 SASS

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
     return $request->user();
 });*/
 
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function(){
-    Route::get('/user', function( Request $request ){
+Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
+    Route::get('/user', function (Request $request) {
         return $request->user();
     });
 });
